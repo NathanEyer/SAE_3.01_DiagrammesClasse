cd2368071b25f36e8ef2e89fe948a041bae2aed6
baki33a <robin.carette6@etu.univ-lorraine.fr>
2024-12-18 11:42:22 +0100

    Modification de ControleurDiagramme et ajout de ControleurImportExport, implémentation du modèle

--- src/main/java/diagrammes/controleur/ControleurDiagramme.java
+++ src/main/java/diagrammes/controleur/ControleurDiagramme.java
@@ -40,14 +40,45 @@ public class ControleurDiagramme implements EventHandler<Event> {
     @Override
     public void handle(Event event) {
         if (event instanceof MouseEvent mouseEvent) {
-            if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED) {
-                System.out.println("Clic à la position : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
-            }
-        }
-        if (event instanceof MouseEvent mouseEvent) {
-            if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-                System.out.println("Déplacement détecté à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+            switch (mouseEvent.getEventType().toString()) {
+                case "MOUSE_CLICKED":
+                    System.out.println("Clic détecté à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+                    // Exemple d'action sur le modèle
+                    modele.selectionnerClasse("Classe");//nom de la classe en question
+                    break;
 
+                case "MOUSE_DRAGGED":
+                    System.out.println("Déplacement détecté à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+                    modele.deplacerClasseSelectionnee(mouseEvent.getX(), mouseEvent.getY());
+                    break;
+
+                case "MOUSE_PRESSED":
+                    System.out.println("Bouton pressé à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+                    // Logique pour initier une action
+                    break;
+
+                case "MOUSE_RELEASED":
+                    System.out.println("Bouton relâché à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+                    // Logique pour finaliser une action
+                    break;
+
+                case "MOUSE_ENTERED":
+                    System.out.println("Souris entrée dans la zone !");
+                    // Logique pour survol d'un élément
+                    break;
+
+                case "MOUSE_EXITED":
+                    System.out.println("Souris sortie de la zone !");
+                    // Logique pour quitter la zone
+                    break;
+
+                case "MOUSE_MOVED":
+                    System.out.println("Mouvement détecté à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+                    // Logique pour mettre en évidence un élément
+                    break;
+
+                default:
+                    System.out.println("Autre événement détecté : " + mouseEvent.getEventType());
             }
         }
     }

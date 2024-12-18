badcf3cded91e7ae969de57a0c30841ff88f2b0f
baki33a <robin.carette6@etu.univ-lorraine.fr>
2024-12-18 11:06:25 +0100

    implémentation de ControlleurDiagramme

--- src/main/java/diagrammes/controleur/ControleurDiagramme.java
+++ src/main/java/diagrammes/controleur/ControleurDiagramme.java
@@ -1,16 +1,54 @@
 package diagrammes.controleur;
 
 import diagrammes.modele.ModeleDiagramme;
+import diagrammes.vue.VueDiagramme;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
-public class ControleurDiagramme implements EventHandler {
+
+/**
+ * ControleurDiagramme est la classe responsable de gérer les interactions
+ *  * entre l'utilisateur, le modèle (ModeleDiagramme) et la vue (VueDiagramme).
+ *  * Elle configure les gestionnaires d'événements pour répondre aux actions de l'utilisateur
+ *  * dans l'interface graphique.
+ */
+public class ControleurDiagramme implements EventHandler<Event> {
+
+
+    /**
+     * Référence au modèle qui contient les données du diagramme.
+     */
+    private ModeleDiagramme modele;
+
+    /**
+     * Constructeur du ControleurDiagramme.
+     * Initialise le contrôleur avec une référence au modèle.
+     *
+     * @param modele Le modèle contenant les données du diagramme.
+     */
     public ControleurDiagramme(ModeleDiagramme modele) {
+        this.modele = modele;
     }
 
-
+    /**
+     * Gère les événements déclenchés par l'utilisateur.
+     * Ici, uniquement les clics de la souris.
+     *
+     * @param event L'événement déclenché par l'utilisateur.
+     */
     @Override
     public void handle(Event event) {
+        if (event instanceof MouseEvent mouseEvent) {
+            if (mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED) {
+                System.out.println("Clic à la position : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+            }
+        }
+        if (event instanceof MouseEvent mouseEvent) {
+            if (mouseEvent.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                System.out.println("Déplacement détecté à : (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
 
+            }
+        }
     }
 }

3820e204fdc874d630805f598ed35ba79ccb8b1e
Joey Rosenkranz <rosenkranzjoey@MacBook-Pro-de-Joey.local>
2024-12-20 09:07:44 +0100

    Création classe Association + Javadocs

--- src/main/java/diagrammes/relations/Association.java
+++ src/main/java/diagrammes/relations/Association.java
@@ -1,17 +1,25 @@
 package diagrammes.relations;//package java;
 
-//public class Association implements RelationStrategy{
-//
-//    public Association() {
-//
-//    }
-//
-//    @Override
-//    public void creerLien() {
-//
-//    }
+public class Association implements RelationStrategy{
 
-//    public static String type(){
-//        return "Association";
-//    }
-//}
+   public Association() {}
+
+   @Override
+   /**
+    * Création d'un lien d'association entre les classes.
+    * @return void
+    */
+    public void creerLien() {
+        System.out.println("Création d'un lien d'association entre les classes.");
+        // Logique spécifique pour créer un lien d'association}
+    }
+
+    /**
+     * Retourne le type de la relation
+     * @return String
+     */
+   public static String type(){
+      return "Association";
+   }
+
+}
--- src/main/java/diagrammes/relations/Relation.java
+++ src/main/java/diagrammes/relations/Relation.java
@@ -13,19 +13,32 @@ public class Relation {
         this.type = type;
     }
 
+    /**
+     * Retourne la classe de départ
+     * @return Classe
+     */
     public Classe getDepart() {
         return depart;
     }
 
+    /**
+     * Retourne la classe de destination
+     * @return Classe
+     */
     public Classe getDestination() {
         return destination;
     }
 
+
+    /**
+     * Retourne le type de relation
+     * @return RelationStrategy
+     */
     public RelationStrategy getType() {
         return type;
     }
 
-    
+
 
 
 }

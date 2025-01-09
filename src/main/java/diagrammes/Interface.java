3d943491f69eca729e65812232c941b6c3686a86
LukaSalvo <luka.salvo23@gmail.com>
2025-01-09 09:09:45 +0100

    Ajout de l'aide

--- src/main/java/diagrammes/Interface.java
+++ src/main/java/diagrammes/Interface.java
@@ -93,8 +93,10 @@ public class Interface {
                 + "- Vous pouvez exporter votre diagramme en différents formats via le menu 'Exporter'.\n"
                 + "- Pour masquer les méthodes ou attributs cliquez deux fois sur n'importe quelle classe'.\n"
                 + "- Pour réinitialiser le diagramme, utilisez l'option 'Réinitialiser' dans le menu 'Éditer'.\n"
-                + "- Pour avoir plus de fonctionnalités, cliquez droit sur une classe et vous pourrez la modifier a votre guise.\n\n"
-                + "Si vous avez des questions supplémentaires, consultez la documentation ou contactez le support.";
+                + "- Pour avoir plus de fonctionnalités, cliquez droit sur une classe et vous pourrez la modifier a votre guise.\n"
+                + "Si vous avez des questions supplémentaires, consultez la documentation ou contactez le support.\n"
+                + "Lorsqu'une classe est de couleur verte, c'est une interface. Lorsqu'elle est de couleur rouge, c'est une classe parent et lorsqu'elle est bleu c'est une classe normale.\n\n"
+                + "Si vous double cliquez sur une classe, alors les attributs et les méthodes seront masqués, si vous double cliquez a nouveau, les attributs et les méthodes se démasqueront.";
 
         TextArea textAreaAide = new TextArea(contenuAide);
         textAreaAide.setEditable(false);
